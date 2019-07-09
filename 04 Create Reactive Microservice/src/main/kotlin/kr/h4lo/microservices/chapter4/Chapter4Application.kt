18b012228131b6365f4797ff5369cd5535697c71
JaehunYoon <goodasd123@gmail.com>
2019-07-09 17:08:50 +0900

    Init

---
+++ 04 Create Reactive Microservice/src/main/kotlin/kr/h4lo/microservices/chapter4/Chapter4Application.kt
@@ -0,0 +1,11 @@
+package kr.h4lo.microservices.chapter4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter4Application
+
+fun main(args: Array<String>) {
+    runApplication<Chapter4Application>(*args)
+}
