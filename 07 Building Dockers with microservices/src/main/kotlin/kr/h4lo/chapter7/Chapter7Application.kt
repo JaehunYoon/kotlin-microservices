82ecce2234618b221828859ba863e549ecbdf44b
JaehunYoon <goodasd123@gmail.com>
2019-08-02 18:38:32 +0900

    Reinitialized (Language : java -> kotlin)

---
+++ 07 Building Dockers with microservices/src/main/kotlin/kr/h4lo/chapter7/Chapter7Application.kt
@@ -0,0 +1,11 @@
+package kr.h4lo.chapter7
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter7Application
+
+fun main(args: Array<String>) {
+	runApplication<Chapter7Application>(*args)
+}
