2fb96eb02821559e7ba28667f4e44ae6eb80eb63
JaehunYoon <goodasd123@gmail.com>
2019-08-02 18:34:41 +0900

    Service discovery

---
+++ 06 Creating Cloud-Native Microservices/Discovery/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
+}
+
+group = "kr.h4lo"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "Greenwich.SR2"
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-devtools")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.clo/**/ud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
---
+++ 07 Building Dockers with microservices/src/main/java/kr/h4lo/chapter7/Chapter7Application.java
@@ -0,0 +1,13 @@
+package kr.h4lo.chapter7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter7Application.class, args);
+	}
+
+}
