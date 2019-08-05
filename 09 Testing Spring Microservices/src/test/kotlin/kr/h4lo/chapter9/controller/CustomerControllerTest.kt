0e762e5ab3289aa52014619c6ed15c1b325e7be2
JaehunYoon <goodasd123@gmail.com>
2019-08-05 17:43:34 +0900

    Mocking beans

--- 09 Testing Spring Microservices/src/test/kotlin/kr/h4lo/chapter9/controller/CustomerControllerTest.kt
+++ 09 Testing Spring Microservices/src/test/kotlin/kr/h4lo/chapter9/controller/CustomerControllerTest.kt
@@ -1,10 +1,17 @@
 package kr.h4lo.chapter9.controller
 
+import kr.h4lo.chapter9.model.Customer
+import kr.h4lo.chapter9.service.CustomerService
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.then
+import org.mockito.Mockito.reset
+import org.mockito.Mockito.times
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -19,31 +26,47 @@ class CustomerControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @MockBean
+    lateinit var customerService: CustomerService
+
     @Test
     fun `mock mvc should be configured`() {
     }
 
     @Test
     fun `we should GET a customer by id`() {
+        given(customerService.getCustomer(1))
+                .willReturn(Customer(1, "mock customer"))
+
         mockMvc.perform(get("/customer/1"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("\$.id").value(1))
-                .andExpect(jsonPath("\$.name").value("Kotlin"))
+                .andExpect(jsonPath("\$.name").value("mock customer"))
                 .andDo(print())
+
+        then(customerService).should(times(1)).getCustomer(1)
+        then(customerService).shouldHaveNoMoreInteractions()
+
+        reset(customerService)
     }
 
     @Test
     fun `we should GET a list of customers`() {
+        given(customerService.getAllCustomers())
+                .willReturn(listOf(Customer(1, "test"), Customer(2, "mocks")))
+
         mockMvc.perform(get("/customers"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("\$").isArray)
                 .andExpect(jsonPath("\$[0].id").value(1))
-                .andExpect(jsonPath("\$[0].name").value("Kotlin"))
+                .andExpect(jsonPath("\$[0].name").value("test"))
                 .andExpect(jsonPath("\$[1].id").value(2))
-                .andExpect(jsonPath("\$[1].name").value("Spring"))
-                .andExpect(jsonPath("\$[2].id").value(3))
-                .andExpect(jsonPath("\$[2].name").value("Microservice"))
+                .andExpect(jsonPath("\$[1].name").value("mocks"))
                 .andDo(print())
 
+        then(customerService).should(times(1)).getAllCustomers()
+        then(customerService).shouldHaveNoMoreInteractions()
+
+        reset(customerService)
     }
 }
