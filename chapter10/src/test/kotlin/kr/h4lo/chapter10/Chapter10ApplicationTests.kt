00fe86923180361731a1a58be21d90c57fd067e8
JaehunYoon <goodasd123@gmail.com>
2019-08-06 11:45:28 +0900

    Init

---
+++ chapter10/src/test/kotlin/kr/h4lo/chapter10/Chapter10ApplicationTests.kt
@@ -0,0 +1,16 @@
+package kr.h4lo.chapter10
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Chapter10ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
